155d7dae14daf85ad29dc5975c0a7a44e627a1ac
agylling <agylling@kth.se>
2019-11-19 16:29:09 +0100

    3d structures (#5)
    * Added 3D models and created prefabs
    
    * Smoothness change
    
    * Made the wall destructable
    
    * resolved feedback

---
+++ agi2/Assets/Scripts/DestroyMesh.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyMesh : MonoBehaviour
+{
+    public GameObject destructableObject; // The mesh with all the sliced parts
+
+    private void OnMouseDown()
+    {
+        GameObject newObj = Instantiate(destructableObject, this.transform.position, this.transform.rotation);
+        newObj.transform.localScale = this.transform.localScale;
+        newObj.transform.SetParent(this.transform.parent);
+        Rigidbody[] objects = newObj.GetComponentsInChildren<Rigidbody>();
+        if (objects.Length != 0)
+        {
+            foreach (Rigidbody current in objects)
+            {
+                current.AddExplosionForce(1000, this.transform.position, 4);
+            }
+        }
+        Destroy(this.gameObject);
+    }
+}
