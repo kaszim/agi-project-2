3951462b23f130c085e2b944579241f1fb0d87e1
liwow <48759368+Liwow@users.noreply.github.com>
2019-11-20 14:28:32 +0100

    Tank shoot (#15) Fixes #9
    * testing tank movement
    
    * tank Movement COMPLETE
    
    * shooting with space

--- agi2/Assets/tankMovement.cs
+++ agi2/Assets/tankMovement.cs
@@ -4,22 +4,28 @@ using UnityEngine;
 
 public class tankMovement : MonoBehaviour
 {
+    //Tank movement related
     [SerializeField] private float maxWheelSpeed;
     [SerializeField] private float wheelDist;
     [SerializeField] private SliderControlInput slider;
     [SerializeField] private float epsilon;
-    [SerializeField] private float zOffset;
+
+    //Bullet related
+    [SerializeField] private GameObject bullet;
+    [SerializeField] private float bulletSpeed;
+    [SerializeField] private Vector3 bulletOrigin;
+
 
     // Start is called before the first frame update
     void Start()
     {
 
-        
     }
 
     // Update is called once per frame
     void Update()
     {
+        //Tankmovement
         Vector2 input = slider.CurrentInputValues;
         input *= maxWheelSpeed;
 
@@ -36,7 +42,14 @@ public class tankMovement : MonoBehaviour
 
             Vector3 pivot = transform.position + transform.right * rotationDist;
             transform.RotateAround(pivot, transform.up, Mathf.Rad2Deg * angularVelocity * Time.deltaTime);
-            
+
+        }
+
+        //Bullet related
+        if (Input.GetKeyDown(KeyCode.Space)) //todo: switch shooting implementation to mobile controls
+        {
+            GameObject activeBullet = Instantiate(bullet, transform.TransformPoint(bulletOrigin), Quaternion.identity);
+            activeBullet.GetComponent <Rigidbody>().velocity = transform.forward * bulletSpeed;
         }
     }
 }
