430c75761854a3b36012c8035f884e11b507f10b
Nathan Bhat <bhatnathan@gmail.com>
2019-11-29 11:18:44 +0100

    Improved AR (#26)
    * Add DS_Store to gitignore
    
    * Add ARCore and make project ARCore friendly
    
    * Build settings
    
    * Proof of concept AR tracking
    
    * Add world prefab
    
    * Add lidgren networking library
    
    * Add beginning of networking
    
    * Add NetworkedGO
    
    * Working network instantiate
    
    * Send updates
    
    * Use FixedUpdate to send updates
    
    * Added first sound files to the project
    
    * Added more sounds and a credits document
    
    * Add rotation
    
    * Kinematics => true if gameobject is not owned
    
    * Added environment, needs testing
    
    * Fix a wall reference
    
    * Add a rock resource
    
    * fixed the terrain so the trees are more clustered and world is less messy
    
    * forgot to add a tree resource to GameWorld
    
    * New artag
    
    * Added Audio Manager
    
    * Prefabify GameWorld
    
    * Changes to Prefab Size
    
    * Working world sending
    
    * Try using tasks again for updates
    
    * Working updates
    
    * Working update on seperate thread
    
    * Add shoot button and tweak UI scale
    
    * Fix a texture format problem
    
    * Added Stop function to audioManager
    
    * Added tank sounds
    
    * Update game world wall references
    
    * Add AudioManager to game world
    
    * Make tank movement disable on update if not owned
    
    * Add mesh destruction from bullets and terrain destruction from tank movement
    
    * Revert "Add mesh destruction from bullets and terrain destruction from tank movement"
    
    This reverts commit 855476a948d10e445d6cacb3cf06c8d17f99039a.
    
    * Made AR better
    
    * Made board follow rotation of image tracked in y-axis
    
    * Added a couple comments
    
    * Edited comment
    
    * Made worldScale a variable

--- agi2/Assets/Scripts/AR/SceneController.cs
+++ agi2/Assets/Scripts/AR/SceneController.cs
@@ -3,26 +3,25 @@ using System.Collections.Generic;
 using GoogleARCore;
 using UnityEngine;
 
-namespace AR
-{
-    public class SceneController : MonoBehaviour
-    {
+namespace AR {
+    public class SceneController : MonoBehaviour {
         public GameObject GameWorldPrefab;
         public GameObject World;
+
+        [SerializeField] private float worldScale;
 
         private List<AugmentedImage> _augmentedImages = new List<AugmentedImage>();
         private GameObject _gameWorld;
 
-        void Start()
-        {
+        private Anchor anchor;
+
+        void Start() {
             QuitOnConnectionErrors();
         }
 
-        void Update()
-        {
+        void Update() {
             // The session status must be Tracking in order to access the Frame.
-            if (Session.Status != SessionStatus.Tracking)
-            {
+            if (Session.Status != SessionStatus.Tracking) {
                 int lostTrackingSleepTimeout = 15;
                 Screen.sleepTimeout = lostTrackingSleepTimeout;
                 return;
@@ -31,30 +30,28 @@ namespace AR
 
             Session.GetTrackables<AugmentedImage>(_augmentedImages, TrackableQueryFilter.Updated);
 
-            foreach (var image in _augmentedImages)
-            {
-                if (image.TrackingState == TrackingState.Tracking && _gameWorld == null)
-                {
+            foreach (var image in _augmentedImages) {
+                if (image.TrackingState == TrackingState.Tracking && _gameWorld == null) {
                     // Create an anchor to ensure that ARCore keeps tracking this augmented image.
-                    Anchor anchor = image.CreateAnchor(image.CenterPose);
-                    World.transform.position = anchor.transform.position;
+                    anchor = image.CreateAnchor(image.CenterPose);
                     _gameWorld = Instantiate(GameWorldPrefab, World.transform);
-                }
-                else if (image.TrackingState == TrackingState.Stopped)
-                {
+                    _gameWorld.transform.localScale = new Vector3(worldScale, worldScale, worldScale); //Scale down a bit so it isn't huge.
+                } else if (image.TrackingState == TrackingState.Stopped) {
                     // TODO: Display something that tracking was lost
                 }
             }
+
+            if (_gameWorld != null && anchor != null) {
+                World.transform.position = anchor.transform.position; //Update position
+                World.transform.eulerAngles = new Vector3 (0, anchor.transform.eulerAngles.y, 0); //Update rotation but keep the world flat so it doesn't mess up gravity.
+            }
+
         }
 
-        void QuitOnConnectionErrors()
-        {
-            if (Session.Status == SessionStatus.ErrorPermissionNotGranted)
-            {
+        void QuitOnConnectionErrors() {
+            if (Session.Status == SessionStatus.ErrorPermissionNotGranted) {
                 Debug.LogError("Camera permission is needed to run this application.");
-            }
-            else if (Session.Status.IsError())
-            {
+            } else if (Session.Status.IsError()) {
                 Debug.LogError("ARCore encountered a problem connecting. Please restart the app.");
             }
         }
