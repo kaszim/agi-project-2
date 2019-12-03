b0db740f2cd692dd98804d0ba24a8c76ef9368d2
agylling <agylling@kth.se>
2019-12-03 09:59:56 +0100

    Flags and divider (#45)

---
+++ agi2/Assets/Scripts/Flagcolor.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Flagcolor : MonoBehaviour
+{
+    [SerializeField] Texture flagTexture;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Renderer render = gameObject.GetComponent<Renderer>();
+        Material mat = new Material(Shader.Find("Standard"));
+        mat.doubleSidedGI = false;
+        mat.mainTexture = flagTexture;
+        mat.enableInstancing = true;
+        render.material = mat;
+    }
+}
